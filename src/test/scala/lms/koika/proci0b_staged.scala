a18105f43e6cd935dcd260b4a26fef4aaf4ce74e
Abdullah <abdullah8a0@gmail.com>
2023-06-27 07:28:33 -0400

    staged proci0

--- src/test/scala/lms/koika/proci0b_staged.scala
+++ src/test/scala/lms/koika/proci0b_staged.scala
@@ -5,8 +5,8 @@ import lms.core.virtualize
 import lms.macros.SourceContext
 
 @virtualize
-class StagedProcInterp1bPC extends TutorialFunSuite {
-  val under = "proci1b_staged_"
+class StagedProcInterp0b extends TutorialFunSuite {
+  val under = "proci0b_staged_"
 
   val regfile_main = """
 int main(int argc, char *argv[]) {
@@ -32,12 +32,11 @@ int main(int argc, char *argv[]) {
     abstract sealed class Instruction
     case class Add(rd: Reg, rs1: Reg, rs2: Reg) extends Instruction
     case class Addi(rd: Reg, rs1: Reg, imm: Int) extends Instruction
-    case class BrNEZ(rs: Reg, imm: Int) extends Instruction
-
+    case class BrTarget(id: String) extends Instruction
+    case class BrNEZ(rs: Reg, id: String) extends Instruction
 
     type Program = List[Instruction]
-    type RegFile = Rep[Array[Int]]
-    type PC = Rep[Int]
+    type RegFile = Array[Int]
 
     case class Reg(id: Int)
     val ZERO: Reg = Reg(0)
@@ -53,34 +52,78 @@ int main(int argc, char *argv[]) {
     implicit def reg2int(r: Reg): Int = r.id
     implicit def reg2rep(r: Reg): Rep[Int] = unit(r.id)
 
-    type State = (RegFile, PC)
+    type State = RegFile
 
     def println(s: String) = if (DEBUG) Predef.println(s) else ()
 
-    def run(prog: Program, state: (RegFile, PC)): RegFile = {
-      val regfile: RegFile = state._1
-      var pc: Var[Int] = __newVar(state._2)
+    def run(prog: Program, state: Rep[State]): Rep[RegFile] = {
+      val regfile: Rep[RegFile] = state
+      val id_to_prog: List[(String, Program)] = prog
+        .filter {
+          case BrTarget(id) => true
+          case _            => false
+        }
+        .map {
+          case BrTarget(id) => (id, prog.dropWhile(_ != BrTarget(id)).tail)
+          case _            => ("", List())
+        }
 
-      while (0 <= readVar(pc) && pc < prog.length) {
-        for (i <- (0 until prog.length): Range) {
-          if (i == pc) {
-            prog(i) match {
-              case Add(rd, rs1, rs2) =>
-                regfile(rd) = regfile(rs1) + regfile(rs2)
-                pc = pc + 1
+      var curblock: Rep[String] = "entry" //
+      var curidx: Rep[Int] = id_to_prog.indexWhere(_._1 == "entry") //
+      val endidx: Int = id_to_prog.indexWhere(_._1 == "exit")
+      var curprog: Program = prog
 
-              case Addi(rd, rs1, imm) =>
-                regfile(rd) = regfile(rs1) + imm
-                pc = pc + 1
+      while (curblock != "exit") {
+        for (i <- (0 until id_to_prog.length): Range) {
+          if (i == curidx) {
+            val (id, curprog) = id_to_prog(i)
+            println(s"curblock at start ${curblock}")
+            println(s"curprog: $curprog")
 
-              case BrNEZ(rs, target) =>
-                if (regfile(rs) != 0) pc = pc + target
-                else pc = pc + 1
+            var break = false
+            for (i <- (0 until curprog.length): Range) {
+              if (!break) {
+                curprog(i) match {
+                  case Add(rd, rs1, rs2) => {
+                    regfile(rd) = regfile(rs1) + regfile(rs2)
+                    println("Add")
+                  }
+
+                  case Addi(rd, rs1, imm) => {
+                    regfile(rd) = regfile(rs1) + imm
+                    println("Addi")
+                  }
+
+                  case BrNEZ(rs, target) =>
+                    if (regfile(rs) != 0) {
+                      println(s"returning target $target")
+                      curblock = target
+                      break = true
+                    } else {
+                      println("not returning target")
+                    }
+
+                  case BrTarget(id) => {
+                    curblock = id
+                    break = true
+                    println(s"target? $id")
+                  }
+                }
+              }
             }
+            println(s"curblock at end ${curblock}")
           }
         }
+        //for (i <- (0 until id_to_prog.length): Range) {
+        //  if (id_to_prog(i)._1 == curblock) {
+            curidx = 2
+            curblock = "exit"
+        //  }
+        //}
+
       }
       regfile
+
     }
   }
 
@@ -124,63 +167,32 @@ int main(int argc, char *argv[]) {
   }
 
   test("proc 1") {
-    val snippet = new DslDriverX[Array[Int], Int] with Interp {
-      override val main = """
-int main(int argc, char *argv[]) {
-  int regfile[6] = {0, 0, 1, 0, 15, -1};
-  int r = Snippet(regfile);
-  printf("%d\n", r);
-  return 0;
-}
-"""
-      def snippet(initRegFile: RegFile) = {
-        val N = A3
-        val Temp = A2
-        val F_n = A1
-        val F_n_1 = A0
-        val MinusOne = A4
-
-        val Fibprog = List(
-          Add(Temp, F_n, F_n_1),
-          Add(F_n_1, F_n, ZERO),
-          Add(F_n, Temp, ZERO),
-          Add(N, N, MinusOne),
-          BrNEZ(N, -4)
-        )
-
-        val res = run(Fibprog, (initRegFile, 0))
-        val expected = Array(0, 610, 987, 987, 0, -1)
-
-        res(F_n)
-      }
-    }
-    check("1", snippet.code)
-  }
-
-  test("proc 2") {
     val snippet = new DslDriverX[Array[Int], Array[Int]] with Interp {
       override val main = regfile_main
-      def snippet(initRegFile: RegFile) = {
+      def snippet(initRegFile: Rep[RegFile]) = {
 
         val N = A3
         val Temp = A2
         val F_n = A1
         val F_n_1 = A0
         val Fibprog = List(
+          BrTarget("entry"),
           Addi(F_n, ZERO, 1),
           Addi(F_n_1, ZERO, 0),
           Addi(N, ZERO, 15),
           Addi(Temp, ZERO, 0),
+          BrTarget("loop"),
           Add(Temp, F_n, F_n_1),
           Add(F_n_1, F_n, ZERO),
           Add(F_n, Temp, ZERO),
           Addi(N, N, -1),
-          BrNEZ(N, -4)
+          BrNEZ(N, "loop"),
+          BrTarget("exit")
         )
 
-        run(Fibprog, (initRegFile, 0))
+        run(Fibprog, (initRegFile))
       }
     }
-    check("2", snippet.code)
+    check("1", snippet.code)
   }
 }
