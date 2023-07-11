ada94af0041f1a8b9239fd74fab9e8ecd2450e5c
Abdullah <abdullah8a0@gmail.com>
2023-07-11 13:14:43 -0400

    fixed multistage

--- src/test/scala/lms/koika/proci0b_staged.scala
+++ src/test/scala/lms/koika/proci0b_staged.scala
@@ -4,8 +4,7 @@ import lms.core.stub._
 import lms.core.virtualize
 import lms.macros.SourceContext
 
-
-// TODO: use topFun in interpcc, look at call. 
+// TODO: use topFun in interpcc, look at call.
 // topFun solves the function call problem. (Prog => (Rep[A=>B]))
 @virtualize
 class StagedProcInterp0b extends TutorialFunSuite {
@@ -33,6 +32,11 @@ int main(int argc, char *argv[]) {
       ret += s"""
   if (regfile[$i] != ${expected(i)}) {
     printf("error: regfile[$i] = %d, expected ${expected(i)}\\n", regfile[$i]);
+    printf("\\nRegfile:\\n");
+    for (int i = 0; i < 6; i++) {
+      printf("%d ", regfile[i]);
+    }
+    printf("\\n\\nFAILED\\n");
     return 1;
   }
 """
@@ -44,6 +48,7 @@ int main(int argc, char *argv[]) {
 """
     ret
   }
+
   override def exec(label: String, code: String, suffix: String = "c") =
     super.exec(label, code, suffix)
 
@@ -56,7 +61,9 @@ int main(int argc, char *argv[]) {
 
     implicit class StringEqualsToOp(s1: Rep[String]) {
       def equalsTo(s2: Rep[String]) = {
-        Wrap[Boolean](Adapter.g.reflect("String.equalsTo", Unwrap(s1), Unwrap(s2)))
+        Wrap[Boolean](
+          Adapter.g.reflect("String.equalsTo", Unwrap(s1), Unwrap(s2))
+        )
       }
     }
 
@@ -65,6 +72,7 @@ int main(int argc, char *argv[]) {
     case class Addi(rd: Reg, rs1: Reg, imm: Int) extends Instruction
     case class BrTarget(id: String) extends Instruction
     case class BrNEZ(rs: Reg, id: String) extends Instruction
+    case class BrNEG(rs: Reg, id: String) extends Instruction
 
     type Program = List[Instruction]
     type RegFile = Array[Int]
@@ -93,18 +101,15 @@ int main(int argc, char *argv[]) {
         .getLines()
         .map { line =>
           val tokens = line.split(" ")
-          tokens(0) match {
-            case "add" =>
-              Add(
-                Reg(tokens(1).toInt),
-                Reg(tokens(2).toInt),
-                Reg(tokens(3).toInt)
-              )
-            case "addi" =>
-              Addi(Reg(tokens(1).toInt), Reg(tokens(2).toInt), tokens(3).toInt)
-            case "br" => BrNEZ(Reg(tokens(1).toInt), tokens(2))
-            case "target" => BrTarget(tokens(1))
-            case _    => println(s"Unknown instruction: $line"); NOP
+          tokens match {
+            case Array("add", rd, rs1, rs2) =>
+              Add(Reg(rd.toInt), Reg(rs1.toInt), Reg(rs2.toInt))
+            case Array("addi", rd, rs1, imm) =>
+              Addi(Reg(rd.toInt), Reg(rs1.toInt), imm.toInt)
+            case Array("brnez", rs, id) => BrNEZ(Reg(rs.toInt), id)
+            case Array("brneg", rs, id) => BrNEG(Reg(rs.toInt), id)
+            case Array("target", id)    => BrTarget(id)
+            case _ => println(s"Unknown instruction: $line"); NOP
           }
         }
         .toList
@@ -133,6 +138,10 @@ int main(int argc, char *argv[]) {
             if (rf(rs) != 0) i = id_to_pc(target)
             else i = i + 1
           }
+          case BrNEG(rs, target) => {
+            if (rf(rs) < 0) i = id_to_pc(target)
+            else i = i + 1
+          }
           case BrTarget(id) => i = i + 1
         }
       }
@@ -149,61 +158,56 @@ int main(int argc, char *argv[]) {
       }
 
       var curblock: Var[String] = __newVar("entry") //
-      // var curidx: Rep[Int] = id_to_prog.indexWhere(_._1 == "entry") //
-      // val endidx: Int = id_to_prog.indexWhere(_._1 == "exit")
       var curprog: Program = prog
       var endprog: Var[Boolean] = false
+      var e2c_dst_r: Var[Int] = __newVar(0)
+      var e2c_val_r: Var[Int] = __newVar(0)
+      var e2c_val_w: Var[Int] = __newVar(0)
+      var e2c_dst_w: Var[Int] = __newVar(0)
       while (!endprog) {
         for (block <- id_to_prog.keys) {
           if (unit(block).equalsTo(readVar(curblock))) {
             curprog = id_to_prog(block)
-            println(s"curblock at start ${curblock}")
-            println(s"curprog: $curprog")
-
             var break = false
             for (i <- (0 until curprog.length): Range) {
               if (!break) {
                 curprog(i) match {
                   case Add(rd, rs1, rs2) => {
-                    regfile(rd) = regfile(rs1) + regfile(rs2)
-                    println("Add")
+                    e2c_dst_w = __newVar(rd.id)
+                    e2c_val_w = __newVar(regfile(rs1) + regfile(rs2))
                   }
 
                   case Addi(rd, rs1, imm) => {
-                    regfile(rd) = regfile(rs1) + imm
-                    println("Addi")
+                    e2c_dst_w = __newVar(rd.id)
+                    e2c_val_w = __newVar(regfile(rs1) + imm)
                   }
 
                   case BrNEZ(rs, target) =>
                     if (regfile(rs) != 0) {
-                      println(s"returning target $target")
                       curblock = target
                       break = true
-                    } else {
-                      println(s"not returning target $target")
+                    }
+                  case BrNEG(rs, target) =>
+                    if (regfile(rs) < 0) {
+                      curblock = target
+                      break = true
                     }
 
                   case BrTarget(id) => {
                     curblock = id
                     break = true
-                    println(s"target? $id")
                   }
                 }
+                regfile(e2c_dst_r) = e2c_val_r
+                e2c_dst_r = e2c_dst_w
+                e2c_val_r = e2c_val_w
               }
             }
-            println(s"curblock at end ${curblock}")
           }
-          if (readVar(curblock).equalsTo(unit("exit"))) {
-            endprog = true
-          }
-
         }
-        // for (i <- (0 until id_to_prog.length): Range) {
-        //  if (id_to_prog(i)._1 == curblock) {
-        //    curblock = "exit"
-        //  }
-        // }
-
+        if (unit("exit").equalsTo(readVar(curblock))) {
+          endprog = true
+        }
       }
       regfile
 
@@ -249,7 +253,6 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  
   test("proc 1") {
     val snippet = new DslDriverX[Array[Int], Array[Int]] with Interp {
       val N = A3
@@ -278,5 +281,22 @@ int main(int argc, char *argv[]) {
       }
     }
     exec("1", snippet.code)
+  }
+
+  ignore("proc stress") {
+    // read from file 0.asm and get the program
+    val snippet = new DslDriverX[Array[Int], Array[Int]] with Interp {
+      val filename = "src/out/0.asm"
+      val program = readProgram(filename)
+      val expected: Array[Int] = expectedResult(program)
+      override val main = constructMain(expected)
+
+      def snippet(initRegFile: Rep[RegFile]) = {
+        run(program, initRegFile)
+        // unit(List(1, 2, 3, 4, 5).toArray)
+      }
+
+    }
+    check("stress", snippet.code)
   }
 }
