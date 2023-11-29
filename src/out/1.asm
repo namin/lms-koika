0d7db00109f1f761b44b78bf893f9a64674595f0
Abdullah <abdullah8a0@gmail.com>
2023-11-29 09:20:14 -0500

    Final

--- src/out/1.asm
+++ src/out/1.asm
@@ -1,8 +1,35 @@
 addi 9 9 5
 addi 9 9 -1
+jumpneg 9 32
+jumpneg 4 15
+add 8 5 5
+addi 4 4 60994
+add 5 2 5
+addi 9 9 -1
+jumpneg 9 26
+jumpneg 8 4
+mul 6 5 1
+addi 6 4 6546
+addi 9 9 -1
+jumpneg 9 21
+jumpneg 2 -8
+mul 4 5 4
+add 4 5 3
+addi 9 9 -1
+jumpneg 9 16
+jumpnz 8 -3
+addi 9 9 -1
+jumpneg 9 13
+jumpneg 4 -7
+add 6 3 1
+sub 6 8 3
+addi 9 9 -1
+jumpneg 9 8
+jumpnz 1 -13
+addi 9 9 -1
 jumpneg 9 5
-jumpnz 6 3
-addi 3 4 28760
-mul 7 2 8
-mul 7 2 1
-mul 5 6 7
+jumpneg 3 -17
+addi 9 9 -1
+jumpneg 9 2
+jumpnz 4 -19
+mul 6 4 8
